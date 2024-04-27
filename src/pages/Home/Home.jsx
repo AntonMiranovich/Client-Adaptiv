adebcccc76aec102c0432d32823d64cecba9dbd9
AntonMiranovich <antonmir992@gmail.com>
2024-04-27 17:00:32 +0300

    add client сдшуте фвфзешм

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import style from "./style.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function Home() {
+
+    return (
+        <div className={style.wrapper}>
+          <h1>hello</h1>
+        </div>
+    );
+}
+
+export default Home;
